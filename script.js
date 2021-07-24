d3ba2bef4eb1e36edf252c89b725f31fd8f91753
saxwaka <48404792+saxwaka@users.noreply.github.com>
2021-07-24 22:21:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,36 +3,33 @@ var string = "";
 var product_id = 0;
 
 var sanpham = [
-  "001-----nike 1-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "002-----nike 2-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "003-----nike 3-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "004-----nike 4-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "005-----nike 5-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "006-----nike 6-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "007-----nike 7-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "008-----nike 8-----36-39-----350-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "009-----nike 9-----36-39-----300-----20-----4-----product/1/0.jpg-----product/1/1.jpg-----product/1/2.jpg-----product/1/3.jpg",
-  "010-----Nike Loang-----36-43-----400-----50-----5-----https://b-f42-zpg.zdn.vn/4353747090640546662/5a715659321ac7449e0b.jpg-----https://f39-zpg.zdn.vn/7286405583677663722/a18ff61c925f67013e4e.jpg-----https://b-f40-zpg.zdn.vn/2852540532100328754/2df3fbf69fb56aeb33a4.jpg-----https://b-f38-zpg.zdn.vn/7633013225544021252/91b9d3f4b7b742e91ba6.jpg-----https://b-f38-zpg.zdn.vn/7504594204968155088/c975557e313dc4639d2c.jpg",
-  "011-----JD panda low-----36->43-----400-----20-----4-----https://f43-zpg.zdn.vn/1890786620495938994/e76826626610914ec801.jpg-----https://f43-zpg.zdn.vn/378664106599094711/2beb87d3c7a130ff69b0.jpg-----https://f38-zpg.zdn.vn/2792505691519474250/9ee135d575a782f9dbb6.jpg-----https://f34-zpg.zdn.vn/3451446942093659183/fec6b3f0f38204dc5d93.jpg",
-  "011-----JD panda low-----36->43-----400-----20-----4-----https://f43-zpg.zdn.vn/1890786620495938994/e76826626610914ec801.jpg-----https://f43-zpg.zdn.vn/378664106599094711/2beb87d3c7a130ff69b0.jpg-----https://f38-zpg.zdn.vn/2792505691519474250/9ee135d575a782f9dbb6.jpg-----https://f34-zpg.zdn.vn/3451446942093659183/fec6b3f0f38204dc5d93.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----4-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----7-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----7-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----7-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----7-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----7-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----7-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg",
+  "1-----nike-----38-----400-----50-----cao cap-----7-----https://f43-zpg.zdn.vn/5331231726879722915/6cfa13441936ee68b727.jpg-----https://f43-zpg.zdn.vn/9011101974931247576/d5262ea524d7d3898ac6.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f19-zpg.zdn.vn/6318559544569110535/77aa4c274655b10be844.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg-----https://f40-zpg.zdn.vn/3726143245847184590/81ae16271c55eb0bb244.jpg",
   
 ];
 
 var bienx = sanpham.length - (tranghientai - 1) * 6;
-var sp1 = sanpham[bienx - 1].split("-----", 15);
-var sp2 = sanpham[bienx - 2].split("-----", 15);
-var sp3 = sanpham[bienx - 3].split("-----", 15);
-var sp4 = sanpham[bienx - 4].split("-----", 15);
-var sp5 = sanpham[bienx - 5].split("-----", 15);
-var sp6 = sanpham[bienx - 6].split("-----", 15);
+var sp1 = sanpham[bienx - 1].split("-----", 20);
+var sp2 = sanpham[bienx - 2].split("-----", 20);
+var sp3 = sanpham[bienx - 3].split("-----", 20);
+var sp4 = sanpham[bienx - 4].split("-----", 20);
+var sp5 = sanpham[bienx - 5].split("-----", 20);
+var sp6 = sanpham[bienx - 6].split("-----", 20);
 var sp = [];
 function hienthisp(x) {
   string = "";
   if (x > 0) {
     string += '<div class="col-xl-4 col-md-6 noidung" onclick="goiham(1)">';
     string += '<a href="#overlay">';
-    string += '<img src="' + sp1[6] + '" alt="Chưa có SP">';
-    string += '<h1 class="h1_text">' + sp1[1] + "</h1>";
+    string += '<img src="' + sp1[7] + '" alt="Chưa có SP">';
+    string += '<h1 class="h1_text">Tên SP: ' + sp1[1] + "</h1>";
+    string += '<h1 class="h1_text">Phân Khúc: ' + sp1[5] + "</h1>";
     string += '<h1 class="h1_text">Giá SP: ' + sp1[3] + ".000vnđ</h1>";
     string += '<h1 class="h1_text">Hoa Hồng CTV: ' + sp1[4] + ".000vnđ</h1>";
     string += "</a>";
@@ -41,8 +38,9 @@ function hienthisp(x) {
   if (x > 1) {
     string += '<div class="col-xl-4 col-md-6 noidung" onclick="goiham(2)">';
     string += '<a href="#overlay">';
-    string += '<img src="' + sp2[6] + '" alt="Chưa có SP">';
-    string += '<h1 class="h1_text">' + sp2[1] + "</h1>";
+    string += '<img src="' + sp2[7] + '" alt="Chưa có SP">';
+    string += '<h1 class="h1_text">Tên SP: ' + sp2[1] + "</h1>";
+    string += '<h1 class="h1_text">Phân Khúc: ' + sp2[5] + "</h1>";
     string += '<h1 class="h1_text">Giá SP: ' + sp2[3] + ".000vnđ</h1>";
     string += '<h1 class="h1_text">Hoa Hồng CTV: ' + sp2[4] + ".000vnđ</h1>";
     string += "</a>";
@@ -51,8 +49,9 @@ function hienthisp(x) {
   if (x > 2) {
     string += '<div class="col-xl-4 col-md-6 noidung" onclick="goiham(3)">';
     string += '<a href="#overlay">';
-    string += '<img src="' + sp3[6] + '" alt="Chưa có SP">';
-    string += '<h1 class="h1_text">' + sp3[1] + "</h1>";
+    string += '<img src="' + sp3[7] + '" alt="Chưa có SP">';
+    string += '<h1 class="h1_text">Tên SP: ' + sp3[1] + "</h1>";
+    string += '<h1 class="h1_text">Phân Khúc: ' + sp3[5] + "</h1>";
     string += '<h1 class="h1_text">Giá SP: ' + sp3[3] + ".000vnđ</h1>";
     string += '<h1 class="h1_text">Hoa Hồng CTV: ' + sp3[4] + ".000vnđ</h1>";
     string += "</a>";
@@ -61,8 +60,9 @@ function hienthisp(x) {
   if (x > 3) {
     string += '<div class="col-xl-4 col-md-6 noidung" onclick="goiham(4)">';
     string += '<a href="#overlay">';
-    string += '<img src="' + sp4[6] + '" alt="Chưa có SP">';
-    string += '<h1 class="h1_text">' + sp4[1] + "</h1>";
+    string += '<img src="' + sp4[7] + '" alt="Chưa có SP">';
+    string += '<h1 class="h1_text">Tên SP: ' + sp4[1] + "</h1>";
+    string += '<h1 class="h1_text">Phân Khúc: ' + sp4[5] + "</h1>";
     string += '<h1 class="h1_text">Giá SP: ' + sp4[3] + ".000vnđ</h1>";
     string += '<h1 class="h1_text">Hoa Hồng CTV: ' + sp4[4] + ".000vnđ</h1>";
     string += "</a>";
@@ -71,8 +71,9 @@ function hienthisp(x) {
   if (x > 4) {
     string += '<div class="col-xl-4 col-md-6 noidung" onclick="goiham(5)">';
     string += '<a href="#overlay">';
-    string += '<img src="' + sp5[6] + '" alt="Chưa có SP">';
-    string += '<h1 class="h1_text">' + sp5[1] + "</h1>";
+    string += '<img src="' + sp5[7] + '" alt="Chưa có SP">';
+    string += '<h1 class="h1_text">Tên SP: ' + sp5[1] + "</h1>";
+    string += '<h1 class="h1_text">Phân Khúc: ' + sp5[5] + "</h1>";
     string += '<h1 class="h1_text">Giá SP: ' + sp5[3] + ".000vnđ</h1>";
     string += '<h1 class="h1_text">Hoa Hồng CTV: ' + sp5[4] + ".000vnđ</h1>";
     string += "</a>";
@@ -81,8 +82,9 @@ function hienthisp(x) {
   if (x > 5) {
     string += '<div class="col-xl-4 col-md-6 noidung" onclick="goiham(6)">';
     string += '<a href="#overlay">';
-    string += '<img src="' + sp6[6] + '" alt="Chưa có SP">';
-    string += '<h1 class="h1_text">' + sp6[1] + "</h1>";
+    string += '<img src="' + sp6[7] + '" alt="Chưa có SP">';
+    string += '<h1 class="h1_text">Tên SP: ' + sp6[1] + "</h1>";
+    string += '<h1 class="h1_text">Phân Khúc: ' + sp6[5] + "</h1>";
     string += '<h1 class="h1_text">Giá SP: ' + sp6[3] + ".000vnđ</h1>";
     string += '<h1 class="h1_text">Hoa Hồng CTV: ' + sp6[4] + ".000vnđ</h1>";
     string += "</a>";
@@ -126,12 +128,13 @@ function anpt() {
   document.getElementById("giasp").innerHTML = sp[3] + ".000 VNĐ";
   document.getElementById("hoahong").innerHTML = sp[4] + ".000 VNĐ";
   document.getElementById("size").innerHTML = sp[2];
+  document.getElementById("phankhuc").innerHTML = sp[5];
   let str1 = "";
   str1 +=
-    '<img src = "'+sp[6]+'" alt = "shoe image"></img>';
+    '<img src = "'+sp[7]+'" alt = "shoe image"></img>';
   document.getElementById("listanh").innerHTML = str1;
 
-  for (let index = 6; index < 6 + Number(sp[5]); index++) {
+  for (let index = 7; index < 7 + Number(sp[6]); index++) {
     string += '<div class="img-item" onclick="anhsp(' + index + ')">';
     string += '<img src="' + sp[index] + '" alt="shoe image" />';
     string += "</div>";
@@ -154,22 +157,22 @@ function loadpage(x) {
   tranghientai = x;
   bienx = sanpham.length - (tranghientai - 1) * 6;
   if (bienx - 1 >= 0) {
-    sp1 = sanpham[bienx - 1].split("-----", 15);
+    sp1 = sanpham[bienx - 1].split("-----", 20);
   }
   if (bienx - 2 >= 0) {
-    sp2 = sanpham[bienx - 2].split("-----", 15);
+    sp2 = sanpham[bienx - 2].split("-----", 20);
   }
   if (bienx - 3 >= 0) {
-    sp3 = sanpham[bienx - 3].split("-----", 15);
+    sp3 = sanpham[bienx - 3].split("-----", 20);
   }
   if (bienx - 4 >= 0) {
-    sp4 = sanpham[bienx - 4].split("-----", 15);
+    sp4 = sanpham[bienx - 4].split("-----", 20);
   }
   if (bienx - 5 >= 0) {
-    sp5 = sanpham[bienx - 5].split("-----", 15);
+    sp5 = sanpham[bienx - 5].split("-----", 20);
   }
   if (bienx - 6 >= 0) {
-    sp6 = sanpham[bienx - 6].split("-----", 15);
+    sp6 = sanpham[bienx - 6].split("-----", 20);
   }
   hienthisp(bienx);
 }
